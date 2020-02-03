b613bb1d9563378cab1ab72b1c7ea444ea0ade95
leejaewoong <dreamsctv@gmail.com>
2020-02-03 19:36:21 +0900

    'spring'

---
+++ CalendarApi/src/main/java/com/tamastudy/calendarApi/domain/common/constant/Constant.java
@@ -0,0 +1,16 @@
+package com.tamastudy.calendarApi.domain.common.constant;
+
+public class Constant {
+
+	public final static String REQ_TOKENERROR_MSG = "request token이 다릅니다.";
+	
+	public final static String REQ_SYSERROR_MSG = "system에러 입니다.";
+	
+	public final static String LOGIN_TOKEN = "token";
+	//line rest api
+	public final static String LINE_API_ENDPOINT = "https://notify-api.line.me/api/notify";
+	//encoding
+	public final static String ENCODEING_TYPE = "UTF-8";
+	//line token
+	public final static String LINE_TOKEN = "LINE_TOKEN";
+}
